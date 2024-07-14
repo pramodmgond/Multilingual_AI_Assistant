99b11ee4add881e242e70c30a7746654e446d917
pramod <pramodgond0075@gmail.com>
2024-07-14 13:33:23 +0530

    requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Pramod',
+    author_email= 'pramodgond0075@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
+
