a2df7a23c72887b764f6f96621428095089b607e
pramod <pramodgond0075@gmail.com>
2024-07-15 23:29:11 +0530

    streamlit app

--- src/helper.py
+++ src/helper.py
@@ -1,75 +1,52 @@
 import speech_recognition as sr
-import google.generativeai as palm
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
 from gtts import gTTS
 
-print("perfect")
+print("perfect!!")
+load_dotenv()
+
+GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
+os.environ["GOOGLE_API_KEY"]=GOOGLE_API_KEY
 
 
 
-# load_dotenv()
-# GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")  
-# os.environ['GOOGLE_API_KEY'] =  GOOGLE_API_KEY
-
-
-# def voice_input():
-#     # Create a recognizer instance
-#     r = sr.Recognizer()
-
-#     with sr.Microphone() as source:
-#         print("Listening...")
-#         audio = r.listen(source)
-
-#     try:
-#         text = r.recognize_google(audio)  # Using Google Speech Recognition
-#         print("You said:", text)
-#         return text
-#     except sr.UnknownValueError:
-#         print("Sorry, could not understand audio")
-#     except sr.RequestError as e:
-#         print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-
-
-
-# def text_to_speech(text):
-#     # Create a gTTS object
-#     tts = gTTS(text=text, lang='en')  # Language can be changed
-
-#     # Save the audio as an MP3 file
-#     tts.save("speech.mp3")
-
-
-
-
-
-# # def llm_model_object(user_text):
-
-# #     model = "models/text-bison-001"
-
-# #     completion = palm.generate_text(
-# #     model=model,
-# #     prompt=user_text,
-# #     temperature=0.3,
-# #     # The maximum length of the response
-# #     max_output_tokens=800,
-# #     )
-
-# #     return completion.result
-
-
-
-
-# def llm_model_object(user_text):
-#     genai.configure(api_key=GOOGLE_API_KEY)
+def voice_input():
+    r=sr.Recognizer()
     
-#     model = genai.GenerativeModel('gemini-pro')
+    with sr.Microphone() as source:
+        print("listening...")
+        audio=r.listen(source)
+    try:
+        text=r.recognize_google(audio)
+        print("you said: ", text)
+        return text
+    except sr.UnknownValueError:
+        print("sorry, could not understand the audio")
+    except sr.RequestError as e:
+        print("could not request result from google speech recognition service: {0}".format(e))
     
-#     response=model.generate_content(user_text)
+
+def text_to_speech(text):
+    tts=gTTS(text=text, lang="en")
     
-#     result=response.text
+    #save the speech from the given text in the mp3 format
+    tts.save("speech.mp3")
+
+def llm_model_object(user_text):
+    #model = "models/gemini-pro"
     
-#     return result
+    genai.configure(api_key=GOOGLE_API_KEY)
+    
+    model = genai.GenerativeModel('gemini-pro')
+    
+    response=model.generate_content(user_text)
+    
+    result=response.text
+    
+    return result
+    
+    
+    
     
